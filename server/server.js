aa497dd29226cd0bc17dc692b1cdad745099fcf1
trungpq164256 <trungpq.sondra@gmail.com>
2021-05-28 21:23:07 +0700

    rm build

--- server/server.js
+++ server/server.js
@@ -53,12 +53,6 @@ Schedule.updateCountForChart();
 const route = require("./routes");
 route(app);
 
-const path = require("path");
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 app.listen(process.env.APP_PORT, () => {
   console.log(`Server is running on port ${process.env.APP_PORT}`);
 });
